d83922c808d0cb31fab5977240b023bf47f51681
MikaGrootenboer <0925620@hr.nl>
2019-12-30 22:19:46 +0100

    variable name changed and class added

--- Main.py
+++ Main.py
@@ -1,11 +1,7 @@
-a = "This is is is a test test senctence"
-head_list = []
+words_input = "This is is is a test test senctence"
+main_list = []
 
-class Splitter:
-    def __init__(self):
-        self.word_counter = 0
-
-    #function that makes a list of words.
+class List_creater:
     def list_maker(self,main_list, word):
         #if there are no words in the list yet place the given word in the list with 1 as times happened
         if (len(main_list)==0):
@@ -27,39 +23,46 @@ class Splitter:
                     main_list.append([word,1])
                     #print("newlist ==",main_list)
                     return main_list
+    
+    def list_sorter(self,main_list): 
+        length = len(main_list) 
+        for i in range(length): 
+            for j in range(0, length-i-1): 
+                if (main_list[j][1] < main_list[j + 1][1]): 
+                    tempo = main_list[j] 
+                    main_list[j]= main_list[j + 1] 
+                    main_list[j + 1]= tempo 
+        return main_list 
 
+class Splitter:
+    def __init__(self):
+        self.word_counter = 0
     #function that splits a sentence.
     def split(self,sentence):
         for i in range(len(sentence)):
             if(sentence[i] == " " and self.word_counter > 0):
-                current_word = a[prev+1:i]
-                self.list_maker(head_list,current_word)
+                current_word = words_input[prev+1:i]
+                List_creater().list_maker(main_list,current_word)
                 prev = i
                 self.word_counter = self.word_counter + 1
             if(sentence[i] == " " and self.word_counter == 0):
                 prev = i
                 first_word = sentence[:prev]
-                self.list_maker(head_list,first_word)
+                List_creater().list_maker(main_list,first_word)
                 self.word_counter = self.word_counter + 1
             if(i+1 == len(sentence)):
                 last_word =sentence[prev+1:]
-                self.list_maker(head_list,last_word)
+                List_creater().list_maker(main_list,last_word)
                 self.word_counter = self.word_counter + 1
 
-test = Splitter()
-test.split(a)
 
-print(head_list)
+#create all the class instances.
+Mainloop = Splitter()
+MainList = List_creater()
 
-def list_sorter(main_list): 
-    length = len(main_list) 
-    for i in range(length): 
-        for j in range(0, length-i-1): 
-            if (main_list[j][1] < main_list[j + 1][1]): 
-                tempo = main_list[j] 
-                main_list[j]= main_list[j + 1] 
-                main_list[j + 1]= tempo 
-    return main_list 
-  
-list_sorter(head_list)
-print(head_list)
+#split and add the words of the variable "words_input" to the head_list
+Mainloop.split(words_input)
+
+
+MainList.list_sorter(main_list)
+print(main_list)
