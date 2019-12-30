b2b9399da7cc46efc36c914d0f3a806403db3736
MikaGrootenboer <0925620@hr.nl>
2019-12-30 21:57:24 +0100

    put the list function in the class

--- Main.py
+++ Main.py
@@ -1,47 +1,49 @@
 a = "This is is a test test senctence"
 head_list = []
 
-def list_maker(main_list, word):
-    if (len(main_list)==0):
-        print(main_list)
-        main_list.append([word,1])
-        print(main_list)
-    else:   
-        for i in range(len(main_list)):
-            if(main_list[i][0]==word):
-                 print("'",word,"'","already in list adding a +1 to the counter")
-                 print("oldlist ==",main_list)
-                 main_list[i][1] = main_list[i][1]+1
-                 print("newlist ==",main_list)
-                 return main_list
-            elif((main_list[i]!=word and i+1 == len(main_list))):
-                print("new word found, adding:..",word)
-                print("oldlist ==",main_list)
-                main_list.append([word,1])
-                print("newlist ==",main_list)
-                return main_list
-
 class Splitter:
     def __init__(self):
         self.word_counter = 0
-        self.wordlist = []
 
-    
+    #function that makes a list of words.
+    def list_maker(self,main_list, word):
+        #if there are no words in the list yet place the given word in the list with 1 as times happened
+        if (len(main_list)==0):
+            main_list.append([word,1])
+        else:   
+            #looping through the list to check all the words.
+            for i in range(len(main_list)):
+                #when a given word is already in the list add the current counter(the second attribute) +1
+                if(main_list[i][0]==word):
+                    #print("'",word,"'","already in list adding a +1 to the counter")
+                    #print("oldlist ==",main_list)
+                    main_list[i][1] = main_list[i][1]+1
+                    #print("newlist ==",main_list)
+                    return main_list
+                #if the word is not in the list add it to the list with a 1 as times happened
+                elif((main_list[i]!=word and i+1 == len(main_list))):
+                    #print("new word found, adding:..",word)
+                    #print("oldlist ==",main_list)
+                    main_list.append([word,1])
+                    #print("newlist ==",main_list)
+                    return main_list
+
+    #function that splits a sentence.
     def split(self,sentence):
         for i in range(len(sentence)):
             if(sentence[i] == " " and self.word_counter > 0):
                 current_word = a[prev+1:i]
-                list_maker(head_list,current_word)
+                self.list_maker(head_list,current_word)
                 prev = i
                 self.word_counter = self.word_counter + 1
             if(sentence[i] == " " and self.word_counter == 0):
                 prev = i
                 first_word = sentence[:prev]
-                list_maker(head_list,first_word)
+                self.list_maker(head_list,first_word)
                 self.word_counter = self.word_counter + 1
             if(i+1 == len(sentence)):
                 last_word =sentence[prev+1:]
-                list_maker(head_list,last_word)
+                self.list_maker(head_list,last_word)
                 self.word_counter = self.word_counter + 1
 
 test = Splitter()
